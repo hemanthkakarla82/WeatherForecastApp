e34c597cc2caeeb364e910a9433d8cc435734db6
hemanthkakarla82 <hemanth.kakarla82@gmail.com>
2020-07-05 11:43:28 -0500

    Updated test cases.

--- src/test/java/com/org/marsh/poc/service/WeatherForecastServiceTest.java
+++ src/test/java/com/org/marsh/poc/service/WeatherForecastServiceTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.assertEquals;
@@ -17,19 +18,19 @@ public class WeatherForecastServiceTest {
 
     private RestTemplate mockRestTemplate;
     private WeatherForecastService weatherForecastService;
-    private String url ="https://api.weatherbit.io/v2.0/current?postal_code=60616&key=f4eb860774724fe1bb9f3db983acb5f5";
 
     @Before
     public void setup(){
         mockRestTemplate = mock(RestTemplate.class);
         weatherForecastService = new WeatherForecastService(mockRestTemplate);
+        ReflectionTestUtils.setField(weatherForecastService,"baseUrl","https://api.weatherbit.io/v2.0");
     }
 
     @Test
     public void testGetWeatherForecastByZipcode(){
         String zipcode = "60616";
-        Object emp = new Object();
-        Mockito.when(mockRestTemplate.getForEntity(url, Object.class)).thenReturn(new ResponseEntity(emp, HttpStatus.OK));
+        String url ="https://api.weatherbit.io/v2.0/current?postal_code=60616&key=f4eb860774724fe1bb9f3db983acb5f5";
+        Mockito.when(mockRestTemplate.getForEntity(url, Object.class)).thenReturn(new ResponseEntity(new Object(), HttpStatus.OK));
         ResponseEntity<?> response = weatherForecastService.getWeatherForecastDataByZipcode(zipcode);
         assertEquals(HttpStatus.OK, response.getStatusCode());
 
@@ -39,12 +40,10 @@ public class WeatherForecastServiceTest {
     public void testGetWeatherForecastByDateRange(){
         String startDate = "2020-07-01";
         String endDate = "2020-07-02";
-        Object emp = new Object();
-        Mockito.when(mockRestTemplate.getForEntity(url, Object.class)).thenReturn(new ResponseEntity(emp, HttpStatus.OK));
+        String url = "https://api.weatherbit.io/v2.0/history/energy?lat=38.00&lon=-78.25&start_date="+startDate+"&end_date="+endDate+"&key=f4eb860774724fe1bb9f3db983acb5f5";
+        Mockito.when(mockRestTemplate.getForEntity(url, Object.class)).thenReturn(new ResponseEntity(new Object(), HttpStatus.OK));
         ResponseEntity<?> response = weatherForecastService.getWeatherForecastHistoryByDateRange(startDate,endDate);
         assertEquals(HttpStatus.OK, response.getStatusCode());
 
     }
-
-
 }
